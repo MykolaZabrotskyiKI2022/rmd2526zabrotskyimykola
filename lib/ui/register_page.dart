5c830987613d881b1a686367977968f3509640fc
MykolaZabrotskyi <mykola.zabrotskyi.ki.2022@lpnu.ua>
2025-12-09 15:17:26 +0200

    lab2

--- lib/ui/register_page.dart
+++ lib/ui/register_page.dart
@@ -11,7 +11,7 @@ class RegisterPage extends StatelessWidget {
   }
 
   void _goToHome(BuildContext context) {
-    Navigator.pushReplacementNamed(context, '/home');
+    Navigator.pop(context, '/login');
   }
 
   @override
